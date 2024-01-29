fc69f6e417f12ac6ae96073c796da49b95387927
gbekoilias <gbekoilias@gmail.com>
2024-01-30 00:21:09 +0100

    analyzing

--- notebook.R
+++ notebook.R
@@ -1,1 +1,17 @@
+#install relevant packages for statistical testing l
+#import the dataset
+data <- read.csv("table_data.csv")
+# Import the dataset
+data <- read.delim("table_data.csv", sep = "\t", header = TRUE)
+library(knitr)
+print(kable(data))
 
+#print out the rows and columns
+print(data)
+
+# Create a contingency table
+contingency_table <- table(data$Group, data$Improvement)
+
+# Perform the chi-square test
+result <- chisq.test(contingency_table)
+print(result)
